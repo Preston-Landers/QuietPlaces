0086cf4e2c140cbd7581604ef8ba12e265acbb41
Preston Landers <planders@utexas.edu>
2014-03-08 12:21:59 -0600

    Add a welcome screen.  Make Android 4.0 (API 14) required.

--- QuietPlaces/src/main/java/edu/utexas/quietplaces/MainActivity.java
+++ QuietPlaces/src/main/java/edu/utexas/quietplaces/MainActivity.java
@@ -52,6 +52,9 @@ public class MainActivity extends ActionBarActivity
         FragmentManager fragmentManager = getSupportFragmentManager();
         Fragment newFragment;
         switch (position) {
+            case 0:
+                newFragment = HomeFragment.newInstance(position + 1);
+                break;
             case 1:
                 newFragment = MapPlaceholderFragment.newInstance(position + 1);
                 break;
@@ -183,6 +186,46 @@ public class MainActivity extends ActionBarActivity
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_map, container, false);
+            return rootView;
+        }
+
+        @Override
+        public void onAttach(Activity activity) {
+            super.onAttach(activity);
+            ((MainActivity) activity).onSectionAttached(
+                    getArguments().getInt(ARG_SECTION_NUMBER));
+        }
+    }
+
+    /**
+     * A fragment containing the welcome screen of the app.
+     */
+    public static class HomeFragment extends Fragment {
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        private static final String ARG_SECTION_NUMBER = "section_number";
+
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+        protected static HomeFragment newInstance(int sectionNumber) {
+            HomeFragment fragment = new HomeFragment();
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        public HomeFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_home, container, false);
             return rootView;
         }
 
--- Readme.md
+++ Readme.md
@@ -2,7 +2,8 @@
 # Welcome to QuietPlace #
 
 This is the source code repository for QuietPlace, an Android app to automatically
-silences your phone when you enter quiet places.
+silence your phone when you enter quiet places. You can create, remove and adjust your
+quiet places at any time.
 
 ## About QuietPlace ##
 
@@ -10,9 +11,10 @@ This is a project for a Mobile Computing class at UT Austin. The goal is to comb
 Google Maps and Places APIs to enable intelligent control over the phone ringer by sensing
  when the user has entered a place categorized as a silent zone, such as hospitals or museums.
 
+
 ## Authors ##
 
-Preston Landers `<planders at utexas dot edu>`
+Preston Landers _planders at utexas dot edu_
 
 Abe Arredondo
 
@@ -20,13 +22,16 @@ Thanks to:
 
 Dr. Christine Julien
 
+
 # Building from Source #
 
 This is a Gradle based project which is buildable from the command line, or from
 Gradle-aware IDEs such as IntelliJ IDEA 13 or Android Studio.  In order to build and run
 the app from source, you must create a new private.xml file and put in a Google API key.
 
+
 ## Add the API Key in res/values/private.xml ##
+
 In order to build from source you must provide an XML file containing the Google Maps API key.
 For security reasons this is not checked into version control. If you wish to build your own
 version of this app you will need to register a new project in the Google API console and obtain
@@ -50,6 +55,7 @@ Replace `PROJECT_NUMBER` with the project number from the Google API console, an
 key you got from the Public API Access area of the console.
 
 `API_KEY` will look something like `AIzaSyBdVl-cTICSwYKrZ95SuvNw7dbMuDt1KG0` (that's not a valid one)
+
 
 ## Debug and Release builds ##
 
